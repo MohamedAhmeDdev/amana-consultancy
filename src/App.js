df5df5368e493073b97107a45c79b688dac11dad
Mohamed Ahmed <ma07041705@gmail.com>
2025-02-06 11:09:40 +0300

    included navbar and footer

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Home from './Pages/Home';
 import About from './Pages/About';
 import FAQs from './Pages/FAQs';
 import Service from './Pages/Service';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/faqs" element={< FAQs />} />
         <Route path="/services" element={< Service />} />
         </Routes>
+        <Footer/>
     </BrowserRouter>
   );
 }
