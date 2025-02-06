9e3c8362bfe1373ae872daaf94023404d97ee3ac
Mohamed Ahmed <ma07041705@gmail.com>
2025-02-06 10:47:30 +0300

    error fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './Pages/Home';
 import About from './Pages/About';
-import Contact from './Pages/Contact';
 import FAQs from './Pages/FAQs';
 import Service from './Pages/Service';
 
