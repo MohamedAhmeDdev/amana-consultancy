f456510b76138d48ef25663c51dcb6b05bf76958
Mohamed Ahmed <ma07041705@gmail.com>
2025-02-06 07:46:27 +0300

    styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: {
+          700: 'rgb(67 87 118)',
+          50: 'rgb(245 247 250)',
+          100: 'rgb(234 238 244)',
+        },
+      },
+      backgroundColor: {
+        'btn-primary': 'rgb(67 87 118)', 
+      },
+      borderColor: {
+        'btn-primary': 'rgb(213 223 233)', 
+      },
+    },
   },
   plugins: [],
 }
